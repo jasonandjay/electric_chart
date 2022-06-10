b7b2515084c0a685976c2cba8b8fb11d630c0ad5
yh <yihang_i@163.com>
2022-06-10 12:20:02 +0800

    feat: add node presenceIndication

--- src/config/config-component-toolbar.ts
+++ src/config/config-component-toolbar.ts
@@ -1,56 +1,93 @@
-import { createToolbarConfig } from '@antv/xflow'
-import { IconStore } from '@antv/xflow'
-import { PlusCircleOutlined } from '@ant-design/icons'
-
-IconStore.set('PlusCircleOutlined', PlusCircleOutlined)
-
-export const useComponentToolbarConfig = createToolbarConfig(toolbarConfig => {
-  /** 生产 toolbar item */
-  toolbarConfig.setToolbarModelService(async toolbarModel => {
-    toolbarModel.setValue(toolbar => {
-      toolbar.mainGroups = [
-        {
-          name: 'main',
-          items: [
-            {
-              id: '1',
-              text: '添加节点',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '电容柜',
-            },
-            {
-              id: '2',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '断路器(竖)',
-            },
-            {
-              id: '3',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '断路器(横)',
-            },
-            {
-              id: '4',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '带电显示',
-            },
-            {
-              id: '5',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '避雷器',
-            },
-            {
-              id: '6',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '电压互感器',
-            },
-            {
-              id: '6',
-              iconName: 'PlusCircleOutlined',
-              tooltip: '接地开关',
-            }
-          ],
-        },
-      ]
-    })
-  })
-})
+import { createToolbarConfig } from '@antv/xflow';
+import { IconStore, XFlowNodeCommands } from '@antv/xflow';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import type { NsNodeCmd } from '@antv/xflow';
+IconStore.set('PlusCircleOutlined', PlusCircleOutlined);
+let id = 1;
+export const useComponentToolbarConfig = createToolbarConfig(
+  (toolbarConfig) => {
+    /** 生产 toolbar item */
+    toolbarConfig.setToolbarModelService(async (toolbarModel) => {
+      toolbarModel.setValue((toolbar) => {
+        toolbar.mainGroups = [
+          {
+            name: 'main',
+            items: [
+              {
+                id: '1',
+                text: '添加节点',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '电容柜',
+                onClick: async ({ commandService }) => {
+                  const nodeName = `Node-${id}`;
+                  commandService.executeCommand<NsNodeCmd.AddNode.IArgs>(
+                    XFlowNodeCommands.ADD_NODE.id,
+                    {
+                      nodeConfig: {
+                        id: nodeName,
+                        label: nodeName,
+                        x: 100 + id * 5,
+                        y: 50 + id * 5,
+                        width: 160,
+                        height: 32,
+                      },
+                    },
+                  );
+                  id += 1;
+                },
+              },
+              {
+                id: '2',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '断路器(竖)',
+              },
+              {
+                id: '3',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '断路器(横)',
+              },
+              {
+                id: '4',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '带电显示',
+                onClick: async ({ commandService }) => {
+                  const nodeName = `Node-${id}`;
+                  commandService.executeCommand<NsNodeCmd.AddNode.IArgs>(
+                    XFlowNodeCommands.ADD_NODE.id,
+                    {
+                      nodeConfig: {
+                        id: nodeName,
+                        renderKey: 'PRESENCEINDICATING',
+                        label: nodeName,
+                        x: 100 + id * 5,
+                        y: 50 + id * 5,
+                        width: 39,
+                        height: 124,
+                      },
+                    },
+                  );
+                  id += 1;
+                },
+              },
+              {
+                id: '5',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '避雷器',
+              },
+              {
+                id: '6',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '电压互感器',
+              },
+              {
+                id: '6',
+                iconName: 'PlusCircleOutlined',
+                tooltip: '接地开关',
+              },
+            ],
+          },
+        ];
+      });
+    });
+  },
+);
--- src/config/config-graph.ts
+++ src/config/config-graph.ts
@@ -1,12 +1,13 @@
-import React from 'react'
-import { createGraphConfig } from '@antv/xflow'
+import React from 'react';
+import { createGraphConfig } from '@antv/xflow';
 /** 自定义React节点/边 */
-import Node1 from '@/components/node1'
-import Node2 from '@/components/node2'
-import Edge1 from '@/components/edge1'
-import Edge2 from '@/components/edge2'
+import Node1 from '@/components/node1';
+import Node2 from '@/components/node2';
+import Edge1 from '@/components/edge1';
+import Edge2 from '@/components/edge2';
+import PRESENCEINDICATING from '@/components/presenceIndicating';
 
-export const useGraphConfig = createGraphConfig(config => {
+export const useGraphConfig = createGraphConfig((config) => {
   /** 设置XFlow画布配置项 */
   config.setX6Config({
     /** 画布网格 */
@@ -22,11 +23,12 @@ export const useGraphConfig = createGraphConfig(config => {
     //   /** 将鼠标位置作为中心缩放 */
     //   zoomAtMousePosition: true,
     // },
-  })
+  });
 
   /** 设置XFlow画布需要渲染的React节点/边 */
-  config.setNodeRender('NODE1', Node1)
-  config.setNodeRender('NODE2', Node2)
-  config.setEdgeRender('EDGE1', Edge1)
-  config.setEdgeRender('EDGE2', Edge2)
-})
+  config.setNodeRender('NODE1', Node1);
+  config.setNodeRender('NODE2', Node2);
+  config.setNodeRender('PRESENCEINDICATING', PRESENCEINDICATING);
+  config.setEdgeRender('EDGE1', Edge1);
+  config.setEdgeRender('EDGE2', Edge2);
+});
